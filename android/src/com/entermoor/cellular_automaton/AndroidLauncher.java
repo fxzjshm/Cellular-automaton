e2f4bab5d544040fc52741ff80e09c57d447179c
fxzjshm <fxzjshm@163.com>
2016-05-21 08:30:23 +0800

    Init

---
+++ android/src/com/entermoor/cellular_automaton/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.entermoor.cellular_automaton;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.entermoor.cellular_automaton.CellularAutomaton;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CellularAutomaton(), config);
+	}
+}
---
+++ html/src/com/entermoor/cellular_automaton/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.entermoor.cellular_automaton.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.entermoor.cellular_automaton.CellularAutomaton;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new CellularAutomaton();
+        }
+}
