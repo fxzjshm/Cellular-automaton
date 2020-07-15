50a3b0a3cabd83f7dde63c020ee3f4d2229c1e4a
fxzjshm <364467005@qq.com>
2020-07-15 19:40:28 +0800

    Better the architecture of asynchronous updaters

---
+++ core/src/com/entermoor/cellular_automaton/updater/AsynchronousUpdater.java
@@ -0,0 +1,25 @@
+package com.entermoor.cellular_automaton.updater;
+
+import com.entermoor.cellular_automaton.CellularAutomaton;
+
+public abstract class AsynchronousUpdater extends CellPoolUpdater {
+    public String platformName, deviceName, updaterName;
+    public int w = 0, h = 0;
+
+    /**
+     * A boolean to ensure this updater is fully initialized before used
+     * @see AsynchronousUpdater#init
+     */
+    public volatile boolean preparing = true;
+
+    /**
+     * asynchronous init, aiming at not blocking the main thread.
+     * Remember to set {@link AsynchronousUpdater#preparing } to false!
+     * @see AsynchronousUpdater#preparing
+     */
+    public abstract void init();
+
+    public AsynchronousUpdater(CellularAutomaton main) {
+        super(main);
+    }
+}
--- core/src/com/entermoor/cellular_automaton/updater/OpenCLUpdater.java
+++ core/src/com/entermoor/cellular_automaton/updater/OpenCLUpdater.java
@@ -5,26 +5,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.entermoor.cellular_automaton.CellularAutomaton;
 
-public abstract class OpenCLUpdater extends CellPoolUpdater {
-    public String platformName, deviceName, updaterName;
-    public int w = 0, h = 0;
+public abstract class OpenCLUpdater extends AsynchronousUpdater {
 
     public static String programSource = "";
-
-    /**
-     * A boolean to ensure this updater is fully initialized before used
-     * @see OpenCLUpdater#init
-     */
-    public volatile boolean preparing = true;
 
     public OpenCLUpdater(CellularAutomaton main) {
         super(main);
     }
 
-    /**
-     * asynchronous init, aiming at not blocking the main thread.
-     * @see OpenCLUpdater#preparing
-     */
     public void init() {
         // since we have had platform id and device id, we don't need to detect them.
         createContext();
